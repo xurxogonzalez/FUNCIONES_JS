aa583c6afd321b054565bb2c98b1588cdfcd762b
Xurxo González <info@webferrol.com>
2021-12-04 11:24:26 +0100

    Añadida función getMin
    -Función getMin() que determina el mínimo de una serie que se pasa como array
    -Añadido etiqueta @description en las descripticones

--- src/functions.js
+++ src/functions.js
@@ -1,7 +1,7 @@
 /**
- * Función que limpia los espacios en blanco: tabulaciones, barra espaciadora, intro
- * @param {String} text Texto bruto con espacios en blanco
- * @returns {String} Texto formateado sin espacios en blanco
+ * @description Función que limpia los espacios en blanco: tabulaciones, barra espaciadora, intro
+ * @param {String} text - Texto bruto con espacios en blanco
+ * @returns {String} - Texto formateado sin espacios en blanco
  */
 // const clearBlankSpaces = text => {    
 //     text = text.replace(/^\s+/,"");
@@ -11,11 +11,18 @@
 const clearBlankSpaces = text => (text = text.replace(/^\s+|\s+$/,"")) && (text = text.replace(/\s+$/,"")) && (text = text.replace(/\s+/g," "));
 
 /**
- * 
- * @param {Number} min Número mínimo de la serie
- * @param {Number} max Número máximo de la serie 
- * @returns {Number} Número aleatorio entre max y min, ambos incluidos
+ * @description Función que devuelve un número aleatorio entre dos números enteros
+ * @param {Number} min - Número entero mínimo de la serie
+ * @param {Number} max - Número entero máximo de la serie 
+ * @returns {Number} - Número aleatorio entre max y min, ambos incluidos
  */
 const getRandom = (min,max) => Math.floor(Math.random()*(max-min+1))+min;
 
- export {clearBlankSpaces,getRandom}; //Las exportaciones por defecto puede coger cualquier nombre
+/**
+ * @description Función que determina el número más pequeño
+ * @param {Object} array - Array de números que determina cuál es más pequeño 
+ * @returns {Number} - El número más pequeño
+ */
+const getMin = array => array.reduce((anterior, actual) => anterior < actual ? anterior : actual);
+
+ export {clearBlankSpaces,getRandom,getMin}; //Las exportaciones por defecto puede coger cualquier nombre
